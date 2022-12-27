bc60452900e34539c1a26dadf53115c3d2d03416
Gordon-Magill <gordon.magill@gmail.com>
2022-12-27 10:37:58 -0800

    Added base client components

---
+++ client/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Nav from "./Nav";
+
+export default function Header() {
+  return (
+    <div className="" id="Header">
+      <h1>Page title</h1>
+      <Nav />
+    </div>
+  );
+}
---
+++ client/src/components/Nav.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Nav() {
+  return (
+    <div className="" id="Nav">
+      <p></p>
+    </div>
+  );
+}
